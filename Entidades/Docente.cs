6d5b18d1f0b1083386ca47742a966c6103970df5
Francisco Navarrete <francisconava125@gmail.com>
2020-05-12 00:00:24 -0300

    PrimerVersion

---
+++ Entidades/Docente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Docente:Personal
+    {
+        private double valorHora;
+
+		#region Propiedades
+		public double ValorHora
+		{
+			get { return this.valorHora; }
+			set { this.valorHora = value; }
+		}
+		public int HorasMensuales
+		{
+			get { return -1; }
+			set { int aux = value; }
+		}
+		#endregion
+		#region Constructor
+		public Docente(string nombre, string apellido, int dni, bool femenino, DateTime horaentrada, DateTime horasalida, double valorho): base(nombre, apellido, dni, femenino,horaentrada,horasalida)
+		{
+			this.valorHora = valorho;
+		}
+
+		#endregion
+
+	
+		public override double CalcularSalario()
+		{
+			return (valorHora * 4) * 20;
+		}
+	}
+}
---
+++ Entidades/Persona.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum ETurno
+    {
+        Mañana,
+        Tarde
+    }
+    public abstract class Persona
+    {
+        private string apellido;
+        private string nombre;
+        private int dni;
+        private bool femenino;
+
+        #region Propiedades
+        public string Apellido
+        {
+            get { return this.apellido; }
+            set { this.apellido = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public int Dni
+        {
+            get { return this.dni; }
+            set { this.dni = value; }
+        }
+        public bool Femenino
+        {
+            get { return this.femenino; }
+            set { this.femenino = value; }
+        }
+        #endregion
+
+        #region Constructores
+
+        public Persona()
+        {
+
+        }
+         public Persona(string nombre, string apellido, int dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+        }
+        public Persona(string nombre, string apellido, int dni, bool femenino):this(nombre,apellido,dni)
+        {
+            this.femenino = femenino;
+        }
+        
+        #endregion
+
+        public override string ToString()
+        {
+            StringBuilder st = new StringBuilder();
+            st.AppendLine("Nombre: " + this.Nombre);
+            st.AppendLine("Apellido: " + this.Apellido);
+            st.AppendLine("Dni: " + this.Dni);
+            st.AppendLine("Genero: " + this.Femenino);
+            return st.ToString();
+
+
+        }
+
+    }
+}
